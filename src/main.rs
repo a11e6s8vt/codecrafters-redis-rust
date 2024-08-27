ab4da4ebaee62309054b3265cf6f8aed193f95d1
Ajeesh Vijayan <ajeesh@astrid001>
2024-08-27 12:08:20 +0100

    multiple clients

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,42 @@
-use std::io::{Read, Write};
-use std::net::{TcpListener, TcpStream};
+use std::io::Result;
+use std::net::SocketAddr;
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{TcpListener, TcpStream},
+};
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+#[tokio::main]
+pub async fn main() -> Result<()> {
+    // Start logging.
+    femme::start();
 
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    // Create TCP Listener
+    let listener = TcpListener::bind("127.0.0.1:6379").await?;
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                println!("accepted new connection");
-                handle_client(stream);
+    // Handle Multiple Clients in a loop
+    loop {
+        let (tcp_stream, socket_addr) = listener.accept().await?;
+        tokio::spawn(async move {
+            match handle_client(tcp_stream, socket_addr).await {
+                Ok(ip) => {
+                    log::info!("{} handle_client terminated gracefully", ip)
+                }
+                Err(error) => log::error!("handle_client encountered error: {}", error),
             }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+        });
     }
 }
 
-fn handle_client(mut stream: TcpStream) {
+async fn handle_client(mut stream: TcpStream, socket_addr: SocketAddr) -> Result<String> {
     let mut buffer = [0; 1024];
-    while let Ok(_input) = stream.read(&mut buffer) {
+    loop {
+        let num_bytes = stream.read(&mut buffer).await?;
         let response = "+PONG\r\n";
 
-        stream.write_all(response.as_bytes()).unwrap();
+        stream.write_all(response.as_bytes()).await?;
+        if num_bytes == 0 {
+            break;
+        }
     }
+    Ok(socket_addr.ip().to_string())
 }
